1c94a74000172d56644277cec597520bd4d7bad0
Eric Tran <40257345+erictran0x@users.noreply.github.com>
2020-11-18 14:01:52 -0800

    Fix spacing inconsistencies (replaced tabs with 4 spaces)

--- finite-lives-helper/FiniteLivesSession.cs
+++ finite-lives-helper/FiniteLivesSession.cs
@@ -11,23 +11,23 @@ namespace Celeste.Mod.FiniteLives
 
         public string HashCode()
         {
-			// Salt the data
-			string salt = "exWOhxnV2KLuSvOdE70k";
-			string str = salt.Substring(10) + LifeCount.ToString() + salt.Substring(0, 6) + InfiniteLives.GetHashCode().ToString() + salt.Substring(3, 8);
+            // Salt the data
+            string salt = "exWOhxnV2KLuSvOdE70k";
+            string str = salt.Substring(10) + LifeCount.ToString() + salt.Substring(0, 6) + InfiniteLives.GetHashCode().ToString() + salt.Substring(3, 8);
 
-			// Create a SHA256 hash from salted data
-			using (SHA256 sha256Hash = SHA256.Create())
-			{
-				// ComputeHash - returns byte array  
-				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+            // Create a SHA256 hash from salted data
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
 
-				// Convert byte array to a string   
-				StringBuilder builder = new StringBuilder();
-				for (int i = 0; i < bytes.Length; i++)
-					builder.Append(bytes[i].ToString("x2"));
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("x2"));
 
-				return builder.ToString();
-			}
-		}
+                return builder.ToString();
+            }
+        }
     }
 }
